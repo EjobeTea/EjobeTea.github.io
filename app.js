6582b5928ced3626e44e7705440d607b0c312322
Ethan Jobe <ejobe98@gmail.com>
2023-01-31 19:19:11 -0600

    Fades in

---
+++ app.js
@@ -0,0 +1,19 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(!entry.isIntersecting){
+            entry.target.style.opacity =  '0';
+            entry.target.style.filter = 'blur(5px)';
+            entry.target.style.transform = 'translateX(-100%)';
+            entry.target.style.transition = 'all 1s';
+        }    
+        else {
+            entry.target.style.opacity =  '1';
+            entry.target.style.filter = 'blur(0)';
+            entry.target.style.transform = 'translateX(0%)';
+        }
+    });
+})
+const hiddenElements = document.querySelectorAll('h1, p');
+hiddenElements.forEach((el)=>observer.observe(el));
+  
